b9c90e551cbb585e6046259da573b1765c40b0e6
Natalia <traffkina@mail.ru>
2023-08-20 16:49:39 +0300

    Initial

---
+++ src/test/java/ru/netology/patterns2/test/AuthTest.java
@@ -0,0 +1,31 @@
+package ru.netology.patterns2.test;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.patterns2.data.DataGenerator.Registration.getRegisteredUser;
+
+class AuthTest {
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    @DisplayName("Should successfully login with active registered user")
+    void shouldSuccessfulLoginIfRegisteredActiveUser() {
+
+        var registeredUser = getRegisteredUser("active");
+
+        $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
+        $("button.button").click();
+        $("h2").shouldHave(Condition.exactText("Личный кабинет")).shouldBe(Condition.visible);
+    }
+
+}
