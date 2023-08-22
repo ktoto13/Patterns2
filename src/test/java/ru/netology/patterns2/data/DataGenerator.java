bdc2101e54da56118d0e4980c098e2f91ec162ef
Natalia <traffkina@mail.ru>
2023-08-22 20:33:29 +0300

    revision

--- src/test/java/ru/netology/patterns2/data/DataGenerator.java
+++ src/test/java/ru/netology/patterns2/data/DataGenerator.java
@@ -6,6 +6,7 @@ import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import lombok.Value;
+
 import java.util.Locale;
 
 import static io.restassured.RestAssured.given;
@@ -72,5 +73,4 @@ public class DataGenerator {
         String password;
         String status;
     }
-
 }
